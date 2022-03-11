35aed09320dbe597dcd5736050fa9f9a5d6f7e9f
jeringeorge97 <101364266+jeringeorge97@users.noreply.github.com>
2022-03-11 07:16:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using dataproggassg2;
+
+Console.WriteLine("Press 1 to sign up\n" +
+    "Press 2 to Login\n" +
+    "Press 3 to view details");
+Console.WriteLine("Enter your option");
+
+int option = int.Parse(Console.ReadLine());
+if(option == 1)
+{
+    Json.Start(@"C:\Users\Kiran\Desktop\New folder\student.json");
+}
+        
+else if(option == 2)
+{
+    Login lg = new Login();
+    lg.login();
+
+}
+else if(option == 3)
+{
+    Console.WriteLine("Enter admin password");
+    string pass=Console.ReadLine();
+    string passwordIsValid = pass = "admin123";
+    bool result = false;
+    if (pass == "admin123")
+    {
+        result = true;
+        viewdetails vd = new viewdetails();
+        vd.read();
+
+    }
+    else
+    {
+        result = false;
+        Console.WriteLine("Enter valid password");
+    }
+    
+
+}
+else
+{
+    Console.WriteLine("enter a valid choice");
+}
+        
+    
+        
+
+
+
+
+
+
+
+
+
---
+++ viewdetails.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataproggassg2
+{
+    internal class viewdetails
+    {
+        string jsondataFromUrl;
+       
+        public void read()
+        {
+            Console.WriteLine("========= Student Data \n");
+
+
+
+            
+            
+                StreamReader reader = new StreamReader(@"C:\Users\Kiran\Desktop\New folder\student.json");
+                jsondataFromUrl = reader.ReadToEnd();
+                if (jsondataFromUrl != null)
+                {
+                    
+                    Console.WriteLine("data------------{0}", jsondataFromUrl);
+                }
+                else
+                {
+                    Console.WriteLine("NO Data----------");
+                }
+            }
+        }
+    }
+
